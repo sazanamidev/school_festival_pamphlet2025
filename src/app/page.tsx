088818722e42efa53a1ec95598aae14812a30337
Asura146 <kyo008g@gmail.com>
2025-10-16 12:39:31 +0900

    useStateフックを追加し、floorの状態を管理するように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 import { Map } from "../components/Map/Map";
 import { ExhibitionInfo } from "../components/ExhibitionInfo/ExhibitionInfo";
 import { EventInfo } from "../components/EventInfo/EventInfo"
+import { useState } from "react";
 
 export default function Home() {
+  const [floor,setFloor] = useState(1);
   return (
     <>
       <Map/>
