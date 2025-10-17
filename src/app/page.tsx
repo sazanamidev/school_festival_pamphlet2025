ac6382967a75ac7a6da7793a80302b96d854b6bb
Asura146 <kyo008g@gmail.com>
2025-10-17 21:09:01 +0900

    MapコンポーネントにsetFloorプロパティを追加し、floorの状態を管理するように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import { Map } from "../components/Map/Map";
 import { ExhibitionInfo } from "../components/ExhibitionInfo/ExhibitionInfo";
@@ -5,10 +6,10 @@ import { EventInfo } from "../components/EventInfo/EventInfo"
 import { useState } from "react";
 
 export default function Home() {
-  const [floor,setFloor] = useState(1);
+  const [floor,setFloor] = useState<number>(1);
   return (
     <>
-      <Map/>
+      <Map setFloor={setFloor}/>
       <ExhibitionInfo/>
       <EventInfo/>
     </>
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,9 @@
-export const Map = () =>{
+'use client'
+type Props = {
+    setFloor: React.Dispatch<React.SetStateAction<number>>;
+  };
+export const Map: React.FC<Props> = ({setFloor}) =>{
+    setFloor(2);
     return(
         <h1>Hello Map</h1>
     );
