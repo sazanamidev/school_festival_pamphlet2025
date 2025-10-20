1a55dd18c68208f3204aa7b5ab23b5be88e809fb
nemukemo <hiroaka201432@gmail.com>
2025-10-20 20:01:50 +0900

    react-swipeableを追加し、Mapコンポーネントにスワイプ操作による階数変更機能を実装

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
+import { useSwipeable } from "react-swipeable";
+
 /**
  * 学園祭のフロアマップを表示するコンポーネント
  * 各階のSVG画像を切り替えて表示する
@@ -21,22 +23,31 @@ export const Map: React.FC<Props> = ({ setFloor }) => {
 		setFloor(floor);
 	}, [floor, setFloor]);
 
+	const handlers = useSwipeable({
+		onSwipedUp:()=>setLocalFloor(f=>Math.max(3,f + 1)),
+		onSwipedDown:()=>setLocalFloor(f=>Math.min(1,f - 1))
+
+	})
+
 	return (
-		<div>
+		<div {...handlers} className="w-full text-center select-none">
 			{/* 現在選択されている階のマップ画像を表示 */}
 			{/* テスト用SVGのURLを指定している */}
 			<Image src={`/test_Map/floor${floor}.svg`} alt={`Floor ${floor}`} width={500} height={500} />
 			
-			{/* 階数選択ボタン */}
-			<button onClick={() => setLocalFloor(0)}>B1階</button>
-			<button onClick={() => setLocalFloor(1)}>1階</button>
-			<button onClick={() => setLocalFloor(2)}>2階</button>
-			<button onClick={() => setLocalFloor(3)}>3階</button>
-			<button onClick={() => setLocalFloor(4)}>4階</button>
-			<button onClick={() => setLocalFloor(5)}>5階</button>
-			<button onClick={() => setLocalFloor(6)}>6階</button>
-			<button onClick={() => setLocalFloor(7)}>7階</button>
-			<button onClick={() => setLocalFloor(8)}>8階</button>
+			<div>
+				{/* 階数選択ボタン */}
+				<button onClick={() => setLocalFloor(0)}>B1階</button>
+				<button onClick={() => setLocalFloor(1)}>1階</button>
+				<button onClick={() => setLocalFloor(2)}>2階</button>
+				<button onClick={() => setLocalFloor(3)}>3階</button>
+				<button onClick={() => setLocalFloor(4)}>4階</button>
+				<button onClick={() => setLocalFloor(5)}>5階</button>
+				<button onClick={() => setLocalFloor(6)}>6階</button>
+				<button onClick={() => setLocalFloor(7)}>7階</button>
+				<button onClick={() => setLocalFloor(8)}>8階</button>
+			</div>
+			
 		</div>
 	);
 };
