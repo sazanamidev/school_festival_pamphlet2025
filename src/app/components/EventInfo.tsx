e3b64ee7c9938704f47f8da5580cf572cca2df1f
TAIYAKI0912 <rinasuketaiyaki@gmail.com>
2025-10-22 20:36:58 +0900

    jsonファイルは完璧かも

--- src/app/components/EventInfo.tsx
+++ src/app/components/EventInfo.tsx
@@ -1,5 +1,22 @@
 export const EventInfo = () =>{
-    return(
-        <h1>Hello EventInfo</h1>
-    );
+
+function showCurrentTime(){
+let now =new Date();
+
+//時、分の数値を取り出す
+let hour =now.getHours();
+let minute=now.getMinutes();
+
+let time="現在時刻"+hour+":"+minute;
+
+setInterval('showCurrentTime',10000);//1分ごとに時刻を取得する関数を実行
+
+switch(true){
+    case(hour===9&&minute===30):
+   
+
+
+
+}
+}
 }
